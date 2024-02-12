396bd251e89b74a057db236b834949298b69a113
Marie Claire Fontaine <mbf5630@psu.edu>
2024-02-11 23:58:45 -0500

    made 2 new cards

--- src/my-card.js
+++ src/my-card.js
@@ -11,21 +11,24 @@ export class MyCard extends LitElement {
     return 'my-card';
   }
 
-  constructor() {
-    super();
-    this.title = 'Chip Default';
-    this.link ="#";
+  static get properties() {
+    return {
+      title: { type: String },
+      link: { type: String },
+      fancy: { type: Boolean, reflect: true },
+    };
   }
+
 
   static get styles() {
     return css`
       :host {
         display: inline-flex;
-       flex-direction: column; 
-       align-items: center; 
-      margin-top: 10px; 
-     }
-
+        flex-direction: column; 
+        align-items: center; 
+        margin-top: 10px; 
+      }
+     
       span {
         background-color: #FFB6C1;
         color: black;
@@ -33,8 +36,6 @@ export class MyCard extends LitElement {
         padding: 7px;
         margin: 4px 0;
       }
-
-
       span:hover {
         background-color: #DB7093;
         border: 2px solid black;
@@ -42,28 +43,47 @@ export class MyCard extends LitElement {
     `;
   }
 
-  render() {
-    return html`<a href="${this.link}"><span @click="${this.handleClick}">${this.title}</span></a>`;
+  constructor() {
+    super();
+    this.fancy = false;
   }
 
-  static get properties() {
-    return {
-      title: { type: String },
-      link:{type: String}
-    };
+  openChanged(e) {
+    console.log(e.newState);
+    if (e.newState === "open") {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
   }
-  connectedCallback() {
-    super.connectedCallback();
-    this.addEventListener('click', this.handleClick.bind(this));
+
+
+
+  render() {
+    return html`
+      <a href="${this.link}">
+        <span @click="${this.handleClick}">${this.title}</span>
+      </a>
+    `;
+  }
+
+
+  render() {
+    return html`
+      <a href="${this.link}">
+        <span @click="${this.handleClick}">${this.title}</span>
+      </a>
+    `;
   }
 
   handleClick() {
     const actions = {
-      "Duplicate Card": () => this.duplicateCard(),
-      "Change Title": () => this.changeTitle(),
-      "Change Image": () => this.changeImage(),
-      "Change Background Color": () => this.changeBgColor(),
-      "Remove Last Card": () => this.removeLastCard()
+      'Duplicate Card': () => this.duplicateCard(),
+      'Change Title': () => this.changeTitle(),
+      'Change Image': () => this.changeImage(),
+      'Change Background Color': () => this.changeBgColor(),
+      'Remove Last Card': () => this.removeLastCard(),
     };
 
     const action = actions[this.title];
@@ -71,27 +91,32 @@ export class MyCard extends LitElement {
   }
 
   duplicateCard() {
+    console.log('Duplicate card clicked');
     const cardList = document.getElementById('cardlist');
     if (cardList.childElementCount < 10) {
-      const lastCard = cardList.lastElementChild;
-      const newCard = lastCard.cloneNode(true);
+      const currentCard = this.closest('.card');
+      const newCard = currentCard.cloneNode(true);
       cardList.appendChild(newCard);
     }
   }
 
   changeTitle() {
+    console.log('Change title clicked');
     const title = this.closest('.card').querySelector('h2');
     title.textContent = 'HAX PSU';
   }
 
   changeImage() {
+    console.log('Change image clicked');
     const card = this.closest('.card');
     const image = card.querySelector('img');
-    image.src = 'https://content.sportslogos.net/logos/33/801/full/penn_state_nittany_lions_logo_secondary_19963011.png';
+    image.src =
+      'https://content.sportslogos.net/logos/33/801/full/penn_state_nittany_lions_logo_secondary_19963011.png';
     image.alt = 'New Image';
   }
 
   changeBgColor() {
+    console.log('Change background color clicked');
     const cards = document.querySelectorAll('.card');
     cards.forEach(function (card) {
       card.classList.toggle('change-bg-color');
@@ -102,6 +127,7 @@ export class MyCard extends LitElement {
   }
 
   removeLastCard() {
+    console.log('Remove last card clicked');
     const cardList = document.getElementById('cardlist');
     if (cardList.childElementCount > 1) {
       cardList.lastElementChild.remove();
